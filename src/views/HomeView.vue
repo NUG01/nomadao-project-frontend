82211faf9fe2dae10afa2920ff8adccb0ecb3115
NUG01 <nugoskhireli@redberry.ge>
2023-01-19 11:45:55 +0400

    fix: bugs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,7 +12,10 @@ export default {
     const dataIsFetched=ref(false)
     const balanceFormActivated=ref(false)
     const withdrawFormActivated=ref(false)
+    const unableWithdraw=ref(false)
+    const rules=ref(false)
     const balance=ref(0)
+    const bet=ref(5)
     const amount=ref('')
     const amountWithdraw=ref('')
     const value=ref('?')
@@ -39,22 +42,27 @@ export default {
 
 
     function play(){
+      notEnough.value=false
       let min = 1;
       let max = 10;
       let random = Math.floor(Math.random() * (max - min + 1)) + min;
-      value.value=random
-      if(random==7){
-        balance.value=2*(balance.value)
+      if(random==7 && (balance.value-bet.value)>=0){
+        value.value=random
+        balance.value=(bet.value*7)+(balance.value)
+      }else if((balance.value-bet.value)>=0){
+        value.value=random
+        balance.value=balance.value-bet.value
       }else{
-        balance.value=balance.value-5
+        notEnough.value=true
       }
       disableGame.value=true
     }
 
     function enableGame(){
       disableGame.value=false
+      notEnough.value=false
       value.value='?'
-      if(balance.value<5){
+      if(balance.value<bet.value){
        notEnough.value=true
       }
     }
@@ -75,14 +83,21 @@ export default {
 
 
    async function withdraw(){
-    if(!(amountWithdraw.value).match(numberRegex) || amountWithdraw.value>=balance.value-5){
-     return
+    unableWithdraw.value=false
+    if(!(amountWithdraw.value).match(numberRegex) || amountWithdraw.value>balance.value){
+      unableWithdraw.value=true
+      return
     }
     const res= await axios.post('withdraw', {
       amount: amountWithdraw.value
       })
       balance.value=res.data
       withdrawFormActivated.value=false
+    }
+
+
+    function changeBetValue(ev){
+      bet.value=Number(ev.target.innerText)
     }
    
 
@@ -100,7 +115,11 @@ export default {
             balanceFormActivated,
             withdrawFormActivated,
             amountWithdraw,
-            withdraw
+            withdraw,
+            bet,
+            changeBetValue,
+            unableWithdraw,
+            rules
        }
   },
 }
@@ -108,6 +127,24 @@ export default {
 
 <template>
   <main v-if="dataIsFetched" class="w-[100vw] h-[100vh] bg-gray-600 flex items-center justify-center">
+    <div v-if="rules" class="w-[100vw] h-[100vh] absolute top-0 left-0 flex items-center justify-center z-30">
+    <div @click="rules=false" class="absolute top-0 left-0 w-[100vw] h-[100vh] bg-[#000] opacity-[0.5] z-40"></div>
+      <div class="w-[50%] bg-gray-300 border-[2px] border-[#000] border-solid py-[2rem] px-[4rem] rounded-[5px] z-50">
+        <ul class="flex flex-col justify-center text-[1.6rem]">
+          <li>◾ You choose how much to bet from the given options ❕</li>
+          <li>◾ If your number is 7, you will get bet multiplied by 7 ❕</li>
+          <li>◾ If your number is not 7, you will lose your bet ❕</li>
+          <li>◾ Your Game = Your Risk ❕</li>
+        </ul>
+      </div>
+    </div>
+    <div v-if="gameStarted" class="flex items-center justify-center absolute top-0 left-0 translate-x-[20%] translate-y-1/2 gap-[1rem]">
+    <div @click="changeBetValue($event)" class="text-[3rem] rounded-[5px] cursor-pointer p-[1rem]" :class="[bet===5 ? 'bg-gray-500 text-[#fff]' : 'bg-gray-200 text-[#000]']" :value="5">5</div>
+    <div @click="changeBetValue($event)" class="text-[3rem] rounded-[5px] cursor-pointer p-[1rem]" :class="[bet===10 ? 'bg-gray-500 text-[#fff]' : 'bg-gray-200 text-[#000]']" :value="10">10</div>
+    <div @click="changeBetValue($event)" class="text-[3rem] rounded-[5px] cursor-pointer p-[1rem]" :class="[bet===20 ? 'bg-gray-500 text-[#fff]' : 'bg-gray-200 text-[#000]']" :value="20">20</div>
+    <div @click="changeBetValue($event)" class="text-[3rem] rounded-[5px] cursor-pointer p-[1rem]" :class="[bet===50 ? 'bg-gray-500 text-[#fff]' : 'bg-gray-200 text-[#000]']" :value="50">50</div>
+    <div @click="changeBetValue($event)" class="text-[3rem] rounded-[5px] cursor-pointer p-[1rem]" :class="[bet===100 ? 'bg-gray-500 text-[#fff]' : 'bg-gray-200 text-[#000]']" :value="100">100</div>
+    </div>
 <div v-if="balanceFormActivated" class="absolute top-0 left-0 w-[100vw] h-[100vh]">
   <div @click="balanceFormActivated=false" class="absolute top-0 left-0 w-[100vw] h-[100vh] bg-[#000] opacity-[0.5] z-30"></div>
 <Form @submit="addBalance" class="absolute bg-gray-500 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 w-[30%] py-[5rem] px-[7rem] rounded-[3px] flex flex-col items-center justify-center">
@@ -120,13 +157,15 @@ export default {
 <Form @submit="withdraw" class="absolute bg-gray-500 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 w-[30%] py-[5rem] px-[7rem] rounded-[3px] flex flex-col items-center justify-center">
   <input v-model="amountWithdraw" type="text" name="amount" id="amount" class="w-[100%] h-[6.3rem] text-[2.4rem] text-[#636363] border-[#cdcdcd] focus:border-[#a7a7a7] focus:border-[1.5px] border-[1px] border-solid rounded-[3px] bg-[#f6f7f7c1] pl-[1.6rem]" />
 <button type="submit" class="flex items-center justify-center bg-[#0095f6] text-[#fff] text-[2.4rem] py-[1rem] font-[500] hover:bg-[#0074cc] px-[3rem] rounded-[5px] mt-[2rem]">Withdraw</button>
+<div @click="unableWithdraw=false" v-if="unableWithdraw" class="text-red-700 text-[1.6rem] text-center mt-[2rem] px-[1rem] py-[0.6rem] bg-[#fff] rounded-[10px] font-500 cursor-pointer">Not enough balance!</div>
 </Form>
 </div>
      <div v-if="gameStarted"  class="absolute right-0 top-0 -translate-x-[7rem] translate-y-[2rem] text-[#fff] text-[2.4rem]">Balance: $<span>{{ balance }}</span></div>
      <div @click="balanceFormActivated=true" v-if="gameStarted"  class="absolute right-0 top-0 -translate-x-[7rem] translate-y-[6rem] text-[#fff] text-[1.6rem] bg-gray-900 px-[1rem] py-[0.6rem] rounded-[7px] cursor-pointer hover:bg-gray-800">Add balance</div>
      <div @click="withdrawFormActivated=true" v-if="gameStarted"  class="absolute right-0 top-0 -translate-x-[7rem] translate-y-[10.5rem] text-[#fff] text-[1.6rem] bg-gray-500 px-[1rem] py-[0.6rem] rounded-[7px] cursor-pointer hover:bg-gray-400">Withdraw</div>
-      <div class="flex flex-col items-center justify-center mb-[5rem]">
+      <div class="flex flex-col items-center justify-center mb-[3rem]">
       <div @click="gameStarted=true" :class="[gameStarted ? 'fadeaway' : '']" class="text-[#fff] text-[3rem] px-[3rem] py-[2rem] bg-[#002] cursor-pointer rounded-[9px] opacity-1">Start Game</div>
+      <div @click="rules=true" :class="[gameStarted ? 'fadeaway' : '']" class="text-[#000] text-[2rem] px-[2rem] py-[1rem] bg-gray-400 cursor-pointer rounded-[9px] opacity-1 mt-[2rem] font-500">Rules</div>
         <p v-if="notEnough" class="text-[3rem] font-[600] text-red-500 mb-[5rem] text-center">Not enough balance! Deposit to continue</p>
         <p @click="enableGame" :class="[gameStarted ? 'fadesymbols' : '']" class="opacity-0 text-[1.6rem] text-gray-300 mb-[2rem] px-[1rem] py-[0.6rem] text-center bg-gray-700 cursor-pointer rounded-[7px]">RELOAD</p>
       <div @click="play" :class="[gameStarted ? 'fadein' : '', balance < 5 ? 'notEnoughBalance' : '', disableGame ? 'noPointer' : '']" class="w-[10rem] h-[0rem] bg-[#693] opacity-0 pointer-events-none flex items-center justify-center rounded-[12px] cursor-pointer">
